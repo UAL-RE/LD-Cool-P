d8ed925ddd34a893ace80e739cb95f27638eff1c
Chun Ly <astro.chun@gmail.com>
2020-03-19 11:28:12 -0700

    figshare_api: Add option for api_token input (iss #1)

--- DataRepository_curation/figshare_api.py
+++ DataRepository_curation/figshare_api.py
@@ -2,12 +2,18 @@ import configparser
 
 from figshare.figshare import Figshare
 
+# Read in default configuration file
 config = configparser.ConfigParser()
 config.read('config/default.ini')
 
 api_token = config.get('global', 'api_token')
 
+if api_token is None or api_token == "***override***":
+    print("ERROR: api_token not available from config file")
+    api_token = input("Provide token through prompt : ")
+
 fs = Figshare(token=api_token, private=True)
+
 
 def curation_retrieve(article_id):
 
