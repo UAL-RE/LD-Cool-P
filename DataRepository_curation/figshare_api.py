29f42d19359c3437bc1d9f5b71a541226942694b
Chun Ly <astro.chun@gmail.com>
2020-03-20 09:40:24 -0700

    figshare_api: Add FigshareAdmin class (iss #1)

--- DataRepository_curation/figshare_api.py
+++ DataRepository_curation/figshare_api.py
@@ -1,6 +1,6 @@
 import configparser
 
-from figshare.figshare import Figshare
+from figshare.figshare import Figshare, issue_request
 
 # Read in default configuration file
 config = configparser.ConfigParser()
@@ -13,6 +13,47 @@ if api_token is None or api_token == "***override***":
     api_token = input("Provide token through prompt : ")
 
 fs = Figshare(token=api_token, private=True)
+
+
+class FigshareAdmin:
+    """ A Python interface to Figshare administration
+
+    Attributes
+    ----------
+    baseurl : str
+        Base URL of the Figshare v2 API
+
+    token : str
+        The Figshare OAuth2 authentication token
+
+    private : bool
+        Boolean to check whether connection is to a private or public article
+
+    Methods
+    -------
+    endpoint(link)
+        Concatenate the endpoint to the baseurl
+
+    get_headers()
+        Return the HTTP header string
+
+    """
+    def __init__(self, token=None, private=False):
+        self.baseurl = "https://api.figshare.com/v2"
+        self.token = token
+        self.private = private
+
+    def endpoint(self, link):
+        """Concatenate the endpoint to the baseurl"""
+        return self.baseurl + link
+
+    def get_headers(self, token=None):
+        """ HTTP header information"""
+        headers = {'Content-Type': 'application/json'}
+        if token:
+            headers['Authorization'] = 'token {0}'.format(token)
+
+        return headers
 
 
 def curation_retrieve(article_id):
