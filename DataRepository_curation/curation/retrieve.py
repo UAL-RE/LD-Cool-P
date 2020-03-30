3ba9895a8007a81fa1c623dd82bb92fc3e634f92
Chun Ly <astro.chun@gmail.com>
2020-03-30 14:13:46 -0700

    curation.retrieve: Add private_file_retrieve; Call within curation_retrieve() (iss #1)

--- DataRepository_curation/curation/retrieve.py
+++ DataRepository_curation/curation/retrieve.py
@@ -1,6 +1,9 @@
 import configparser
 
+from urllib.request import Request, urlopen
+
 from figshare.figshare import Figshare  # , issue_request
+from ..figshare_api import FigshareInstituteAdmin
 
 # Read in default configuration file
 config = configparser.ConfigParser()
@@ -14,8 +17,31 @@ if api_token is None or api_token == "***override***":
 
 fs = Figshare(token=api_token, private=True)
 
+# fs_admin = FigshareInstituteAdmin(token=api_token)
+
+
+def private_file_retrieve(url, filename=None, token=None):
+
+    req = Request(url)
+    if token:
+        req.add_header('Authorization', 'token {}'.format(token))
+
+    response = urlopen(req)
+    content = response.read()
+    print(url)
+
+    f = open(filename, 'wb')
+    f.write(content)
+    f.close()
+
 
 def curation_retrieve(article_id):
 
     # Retrieve article information
     article_details = fs.get_article_details(article_id)
+
+    file_list = fs.list_files(article_id)
+
+    for file_dict in file_list:
+        private_file_retrieve(file_dict['download_url'],
+                              filename=file_dict['name'], token=api_token)
