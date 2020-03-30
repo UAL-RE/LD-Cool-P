b56c35715aebd76229968bbfe1b3fe606cf66ff4
Chun Ly <astro.chun@gmail.com>
2020-03-30 14:39:06 -0700

    curation.retrieve.curation_retrieve: Add directory paths to write files (iss #1)

--- DataRepository_curation/curation/retrieve.py
+++ DataRepository_curation/curation/retrieve.py
@@ -1,13 +1,13 @@
+import os
 import configparser
-
 from urllib.request import Request, urlopen
 
 from figshare.figshare import Figshare  # , issue_request
-from ..figshare_api import FigshareInstituteAdmin
+# from ..figshare_api import FigshareInstituteAdmin
 
 # Read in default configuration file
 config = configparser.ConfigParser()
-config.read('config/default.ini')
+config.read('DataRepository_curation/config/default.ini')
 
 api_token = config.get('global', 'api_token')
 
@@ -35,13 +35,24 @@ def private_file_retrieve(url, filename=None, token=None):
     f.close()
 
 
-def curation_retrieve(article_id):
+def curation_retrieve(article_id, root_directory=None, data_directory=None):
+
+    if root_directory is None:
+        root_directory = os.getcwd()
 
     # Retrieve article information
     article_details = fs.get_article_details(article_id)
 
     file_list = fs.list_files(article_id)
 
+    if not data_directory:
+        dir_path = os.path.join(root_directory, "figshare_{0}/".format(article_id))
+    else:
+        dir_path = os.path.join(root_directory, data_directory)
+
+    os.makedirs(dir_path, exist_ok=True)  # This might require Python >=3.2
+
     for file_dict in file_list:
-        private_file_retrieve(file_dict['download_url'],
-                              filename=file_dict['name'], token=api_token)
+        filename = os.path.join(dir_path, file_dict['name'])
+        private_file_retrieve(file_dict['download_url'], filename=filename,
+                              token=api_token)
