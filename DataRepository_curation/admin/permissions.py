b08c9660ae7790fc128b1e5f343d92bd2473af94
Chun Ly <astro.chun@gmail.com>
2020-04-07 15:23:55 -0700

    admin: Add permission module for data curation (iss #6)

---
+++ DataRepository_curation/admin/permissions.py
@@ -0,0 +1,18 @@
+"""
+A set of functions to change permissions for a given path or file for data
+curation backend support
+"""
+
+from os.path import join
+from os import chmod, walk
+
+
+def curation(path):
+    for dir_path, dir_names, files in walk(path):
+        for dir_name in dir_names:
+            # print(join(root, file))
+            chmod(join(dir_path, dir_name), 0o777)
+
+        for file in files:
+            # print(join(root, file))
+            chmod(join(dir_path, file), 0o777)
