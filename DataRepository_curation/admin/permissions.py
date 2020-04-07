d135f5f3d7bf194a309744b0d611ac8a6b42a8ea
Chun Ly <astro.chun@gmail.com>
2020-04-07 15:37:25 -0700

    admin.permissions.curation: Update docstrings (iss #6, #2)

--- DataRepository_curation/admin/permissions.py
+++ DataRepository_curation/admin/permissions.py
@@ -8,6 +8,13 @@ from os import chmod, walk
 
 
 def curation(path):
+    """
+    Purpose:
+      Set permissions to rwx for all folders and files under the
+      parent location of [path]
+
+    :param path: Parent location path
+    """
     for dir_path, dir_names, files in walk(path):
         for dir_name in dir_names:
             # print(join(root, file))
