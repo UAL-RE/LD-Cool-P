b3776724073b6154f7196965547ec56ff7c4c4f3
Chun Ly <astro.chun@gmail.com>
2020-04-08 12:54:44 -0700

    admin.permissions: Add curation_owner() function (iss #6)

--- DataRepository_curation/admin/permissions.py
+++ DataRepository_curation/admin/permissions.py
@@ -5,7 +5,7 @@ curation backend support
 
 from os.path import join, isdir
 
-from os import chmod, walk
+from os import chmod, chown, walk
 
 
 def curation(path):
@@ -28,3 +28,25 @@ def curation(path):
         for file in files:
             # print(join(root, file))
             chmod(join(dir_path, file), 0o777)
+
+
+def curation_owner(path, user):
+    """
+    Purpose:
+      Change user and group ownership of all folders and files
+      under the parent location of [path]
+
+    :param path: Parent location path
+    """
+
+    group = ''  # Placeholder
+
+    if isdir(path):
+        chown(path, user, group)
+
+    for dir_path, dir_names, files in walk(path):
+        for dir_name in dir_names:
+            chown(join(dir_path, dir_name), user, group)
+
+        for file in files:
+            chown(join(dir_path, file), user, group)
