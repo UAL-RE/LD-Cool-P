a940805b6acc8ca5dc6fd7f97e29c06827ad5494
Chun Ly <astro.chun@gmail.com>
2020-04-23 16:27:27 -0700

    curation.inspection.readme.strip.strip_html_comments: Update docstring, Add status print statement; if statement indent (iss #15)

--- DataRepository_curation/curation/inspection/readme/strip.py
+++ DataRepository_curation/curation/inspection/readme/strip.py
@@ -15,25 +15,28 @@ def strip_html_comments(lines0, change):
     :param lines0: list containing text from open()
     :param change: int that indicates whether changes have been implemented
 
-    :return lines: list containing text with HTML comments stripped out
+    :return lines: np.array containing text with HTML comments stripped out
     :return change: Updated change (+1) if changes are made
     """
+
+    lines = np.array(lines0)
 
     # Strip out HTML comments noted via <!--- to -->
     html_comment_beg = [xx for xx in range(len(lines0)) if '<!---' in lines0[xx][0:5]]
     html_comment_end = [xx for xx in range(len(lines0)) if '-->' in lines0[xx][0:3]]
 
     if len(html_comment_beg) != 0:
+        print("HTML comments found. Stripping...")
         change += 1
 
-    list_range = [[*range(beg, end+1)] for beg, end in zip(html_comment_beg, html_comment_end)]
+        list_range = [[*range(beg, end+1)] for beg, end in
+                      zip(html_comment_beg, html_comment_end)]
 
-    # Concatenate list_range into a single list of index to remove
-    remove_index = [j for i in list_range for j in i]
+        # Concatenate list_range into a single list of index to remove
+        remove_index = [j for i in list_range for j in i]
 
-    # Easier to use numpy to remove a list of index than using list remove()
-    lines = np.array(lines0)
-    lines = np.delete(lines, remove_index)
+        # Easier to use numpy to remove a list of index than using list remove()
+        lines = np.delete(lines, remove_index)
 
     return lines, change
 
