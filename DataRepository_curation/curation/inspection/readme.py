2d667c24efd8b8a7965c87bec452f6d3c113c68e
Chun Ly <astro.chun@gmail.com>
2020-04-23 10:57:30 -0700

    curation.inspection.readme: Refactor html comment stripping into strip_html_comments() function; Call function in strip_comments() (iss #15)

--- DataRepository_curation/curation/inspection/readme.py
+++ DataRepository_curation/curation/inspection/readme.py
@@ -100,15 +100,17 @@ def retrieve(depositor_name):
         print("Default README file found! Not overwriting with template!")
 
 
-def strip_comments(depositor_name):
+def strip_html_comments(lines0, change):
+    """
+    Purpose:
+      Remove HTML comments from README.txt file
 
-    README_file_default, _ = default_readme_path(depositor_name)
+    :param lines0: list containing text from open()
+    :param change: int that indicates whether changes have been implemented
 
-    f1 = open(README_file_default, 'r')
-    lines0 = f1.readlines()
-    f1.close()
-
-    change = 0
+    :return lines: list containing text with HTML comments stripped out
+    :return change: Updated change (+1) if changes are made
+    """
 
     # Strip out HTML comments noted via <!--- to -->
     html_comment_beg = [xx for xx in range(len(lines0)) if '<!---' in lines0[xx][0:5]]
@@ -119,10 +121,27 @@ def strip_comments(depositor_name):
 
     list_range = [[*range(beg, end+1)] for beg, end in zip(html_comment_beg, html_comment_end)]
 
+    # Concentage list_range into a single list of index to remove
     remove_index = [j for i in list_range for j in i]
 
+    # Easier to use numpy to remove a list of index than using list remove()
     lines = np.array(lines0)
     lines = np.delete(lines, remove_index)
+
+    return lines, change
+
+
+def strip_comments(depositor_name):
+
+    README_file_default, _ = default_readme_path(depositor_name)
+
+    f1 = open(README_file_default, 'r')
+    lines0 = f1.readlines()
+    f1.close()
+
+    change = 0
+
+    lines, change = strip_html_comments(lines0, change)
 
     if change:
         print(README_file_default)
