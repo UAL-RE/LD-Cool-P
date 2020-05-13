2197ec8498b591fc2bf01200e793c9b892ef1167
Chun Ly <astro.chun@gmail.com>
2020-05-13 13:19:10 -0700

    curation.depositor_name.DepositorName: Update docstrings (iss #2, #28)

--- DataRepository_curation/curation/depositor_name.py
+++ DataRepository_curation/curation/depositor_name.py
@@ -2,6 +2,39 @@ from . import df_to_dict_single
 
 
 class DepositorName:
+    """
+    Purpose:
+      Retrieve depositor information for a deposit
+
+    Attributes
+    ----------
+    article_id : int
+      Figshare article ID
+
+    fs_admin :
+      Figshare Admin object
+
+    cur_df : pandas DataFrame
+      pandas DataFrame containing full list of curation items
+
+    acct_df : pandas DataFrame
+      pandas DataFrame containing Figshare Institution accounts
+
+    cur_loc_dict : dictionary
+      dictionary containing general curation information
+
+    curation_dict : dictionary
+      dictionary containing detailed curation information
+
+    Methods
+    -------
+    get()
+      Retrieve dictionary of depositor name information
+
+    depositor_folder_name()
+      Retrieve string containing preferred curation folder name for deposit
+    """
+
     def __init__(self, article_id, fs_admin):
         self.article_id = article_id
         self.cur_df = fs_admin.get_curation_list()
@@ -9,6 +42,7 @@ class DepositorName:
 
         self.cur_loc_dict = df_to_dict_single(self.cur_df.loc[self.cur_df['article_id'] == self.article_id])
         self.curation_dict = fs_admin.get_curation_details(self.cur_loc_dict['id'])
+
         self.depositor_dict = self.get()
         self.depositor_folderName = self.depositor_folder_name()
 
