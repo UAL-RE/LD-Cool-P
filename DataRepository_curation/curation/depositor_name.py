60e20214d0efda7b619ee5cb5697176e63cad2fd
Chun Ly <astro.chun@gmail.com>
2020-05-14 10:54:35 -0700

    curation.depositor_name.DepositorName: Change dictionary key names for simplicity (iss #28)

--- DataRepository_curation/curation/depositor_name.py
+++ DataRepository_curation/curation/depositor_name.py
@@ -26,6 +26,12 @@ class DepositorName:
     curation_dict : dictionary
       dictionary containing detailed curation information
 
+    name_dict : dictionary
+      Dictionary containing all possible permutation of depositor name
+
+    folderName: str
+      Preferred folder name for data curation process given article information
+
     Methods
     -------
     get()
@@ -43,8 +49,8 @@ class DepositorName:
         self.cur_loc_dict = df_to_dict_single(self.cur_df.loc[self.cur_df['article_id'] == self.article_id])
         self.curation_dict = fs_admin.get_curation_details(self.cur_loc_dict['id'])
 
-        self.depositor_dict = self.get()
-        self.depositor_folderName = self.depositor_folder_name()
+        self.name_dict  = self.get()
+        self.folderName = self.folder_name()
 
     def get(self):
         print("Retrieving depositor_name for {} ... ".format(self.article_id))
@@ -52,36 +58,32 @@ class DepositorName:
         account_id = self.curation_dict['account_id']
         temp_dict = df_to_dict_single(self.acct_df.loc[self.acct_df['id'] == account_id])
 
-        depositor_surName       = temp_dict['last_name']   # full last name
-        depositor_firstName     = temp_dict['first_name']  # full first name
-        depositor_simplifyFirst = depositor_firstName.split(' ')[0]
-        depositor_simplifySur   = depositor_surName.split(' ')[0]
-        depositor_fullName      = "{} {}".format(depositor_firstName,
-                                                 depositor_surName)
-        depositor_simplifyName  = "{} {}".format(depositor_simplifyFirst,
-                                                 depositor_simplifySur)
+        surName            = temp_dict['last_name']   # full last name
+        firstName          = temp_dict['first_name']  # full first name
+        simplify_firstName = firstName.split(' ')[0]
+        simplify_surName   = surName.split(' ')[0]
+        fullName           = "{} {}".format(firstName, surName)
+        simplify_fullName  = "{} {}".format(simplify_firstName, simplify_surName)
 
-        depositor_dict = dict()
-        depositor_dict['depositor_surName']       = depositor_surName
-        depositor_dict['depositor_firstName']     = depositor_firstName
-        depositor_dict['depositor_simplifyFirst'] = depositor_simplifyFirst
-        depositor_dict['depositor_simplifySur']   = depositor_simplifySur
-        depositor_dict['depositor_fullName']      = depositor_fullName
-        depositor_dict['depositor_simplifyName']  = depositor_simplifyName
+        name_dict = dict()
+        name_dict['surName']   = surName
+        name_dict['firstName'] = firstName
+        name_dict['simplify_firstName'] = simplify_firstName
+        name_dict['simplify_surName']   = simplify_surName
+        name_dict['fullName']           = fullName
+        name_dict['simplify_fullName']  = simplify_fullName
 
-        return depositor_dict
+        return name_dict
 
-    def depositor_folder_name(self):
+    def folder_name(self):
         # Check to see if the depositor is in the list of authors
         authors = [d['full_name'] for d in self.curation_dict['item']['authors']]
-        if self.depositor_dict['depositor_fullName'] in authors or \
-                self.depositor_dict['depositor_simplifyName'] in authors:
+        if self.name_dict['fullName'] in authors or \
+                self.name_dict['simplify_fullName'] in authors:
             print("  Depositor == author")
-            depositor_folderName = self.depositor_dict['depositor_simplifyName']
+            folderName = self.name_dict['simplify_fullName']
         else:
             print("  Depositor != author")
-            depositor_folderName = \
-                '{} - {}'.format(self.depositor_dict['depositor_simplifyName'],
-                                 authors[0])
-        print("depository_name : {}".format(depositor_folderName))
-        return depositor_folderName
+            folderName = '{} - {}'.format(self.name_dict['simplify_fullName'], authors[0])
+        print("depository_name : {}".format(folderName))
+        return folderName
