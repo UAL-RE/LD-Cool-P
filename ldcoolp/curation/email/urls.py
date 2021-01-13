c240db3baa808f9cf29c93c72128763cbb4590f9
Chun Ly <astro.chun@gmail.com>
2021-01-13 08:10:05 -0700

    urls: Handle existing TinyURL alias

--- ldcoolp/curation/email/urls.py
+++ ldcoolp/curation/email/urls.py
@@ -29,14 +29,25 @@ def tiny_url(url, alias=None, log=None):
     if alias is not None:
         params = {'alias': alias}
 
-    response = requests.get(get_url, params=params)
+    expected_url = f"https://tinyurl.com/{alias}"
 
-    try:
-        response.raise_for_status()
-        response_data = response.text
-    except HTTPError as error:
-        log.warning(f"Caught an HTTPError: {error}")
-        log.warning('Body:\n', response.text)
-        raise
+    # Note that if the input URL changes, the code currently does not
+    # recognize that.
+    expected_response = requests.get(expected_url)
+    if expected_response.status_code == 200:
+        log.info(f"TinyURL link already exists!")
+        response_data = expected_url
+    else:
+        log.info(f"TinyURL link does not exist. Creating!")
+        # GET still works if the TinyURL alias exists, but points to the same URL
+        response = requests.get(get_url, params=params)
+
+        try:
+            response.raise_for_status()
+            response_data = response.text
+        except HTTPError as error:
+            log.warning(f"Caught an HTTPError: {error}")
+            log.warning('Body:\n', response.text)
+            raise HTTPError
 
     return response_data
