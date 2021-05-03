7b29ca6962d48b3c2a42ecbdff98006f344bcc2b
Chun Ly <astro.chun@gmail.com>
2021-05-03 11:27:03 -0700

    Bump version: v1.0.2 -> v1.0.3

--- ldcoolp/__init__.py
+++ ldcoolp/__init__.py
@@ -1,6 +1,6 @@
 from os import path
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 CODE_NAME = "LD-Cool-P"
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='ldcoolp',
-    version='v1.0.2',
+    version='v1.0.3',
     packages=['ldcoolp'],
     url='https://github.com/ualibraries/LD_Cool_P',
     license='MIT License',
