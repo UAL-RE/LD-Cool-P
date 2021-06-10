9a42cd43977f21b85beee6e205edd6e5f7522455
Chun Ly <astro.chun@gmail.com>
2021-06-10 09:47:51 -0700

    Bump version: v1.1.0 -> v1.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='ldcoolp',
-    version='1.1.0',
+    version='1.1.1',
     packages=['ldcoolp'],
     url='https://github.com/UAL-ODIS/LD-Cool-P',
     license='MIT License',
