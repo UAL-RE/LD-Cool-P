2bccd85d7220e4bdc83a906f3d31afa28d33470d
Chun Ly <astro.chun@gmail.com>
2021-07-19 14:54:49 -0700

    Bump version: v1.1.4 -> v1.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='ldcoolp',
-    version='1.1.4',
+    version='1.1.5',
     packages=['ldcoolp'],
     url='https://github.com/UAL-RE/LD-Cool-P',
     license='MIT License',
